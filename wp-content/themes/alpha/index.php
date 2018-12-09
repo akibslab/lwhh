250492e7e740668c4f42c59cf8239c7074570275
Akib Hossain <akib.hossain.iub@gmail.com>
2018-12-09 19:40:01 +0600

    Make Post Dynamic

--- wp-content/themes/alpha/index.php
+++ wp-content/themes/alpha/index.php
@@ -1,18 +1,96 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title><?php bloginfo("name") ?></title>
+    <meta charset="UTF-8">
+    <?php wp_head(); ?>
 </head>
-<body>
-
-	<?php
-		while (have_posts()) {
+<body <?php body_class(); ?>>
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="tagline">
+                	<?php bloginfo("description"); ?>
+                </h3>
+                <h1 class="align-self-center display-1 text-center heading">
+                	<?php bloginfo("name"); ?>
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="posts">
+	<?php 
+		while (have_posts()) :
 			the_post();
-			echo "<h2>";
-			the_title();
-			echo "</h2>";
-		}
-	?>
+			?>
+				<div class="post" <?php post_class(); ?>>
+			        <div class="container">
+			            <div class="row">
+			                <div class="col-md-12">
+			                    <h2 class="post-title"><?php the_title(); ?></h2>
+			                </div>
+			            </div>
+			            <div class="row">
+			                <div class="col-md-4">
+			                    <p>
+			                        <strong><?php the_author(); ?></strong><br/>
+			                        <?php the_date(); ?>
 
+			                    </p>
+			                    
+			                        <?php echo get_the_tag_list( $before = '<ul class="list-unstyled"><li>', $sep = '</li><li>', $after = '</li></ul>', $id = 0 ); ?>
+			                </div>
+			                <div class="col-md-8">
+			                    <p>
+			                        <?php
+			                        	if(has_post_thumbnail()) {
+			                        		the_post_thumbnail("large","class=img-fluid");
+			                        	}
+			                        ?>
+			                    </p>
+			                    <p>
+			                    	<?php the_excerpt(); ?>
+			                    </p>
+
+			                    
+			                </div>
+			            </div>
+
+			        </div>
+			    </div>
+			<?php
+
+		endwhile;
+	?>
+</div>
+
+
+<div class="container mb-5">
+	<div class="row">
+		<div class="col-sm-8 offset-sm-4">
+			<?php 
+            	the_posts_pagination( array( 
+            		'screen_reader_text' => ' ',
+            		'next_text' => 'New Post',
+            		'prev_text' => 'Old Post'
+            	 ) );
+            ?>
+		</div>
+	</div>
+</div>
+
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; LWHH - All Rights Reserved
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php wp_footer(); ?>
 </body>
 </html>
